28955eee206fc4ab3beed0e55da2f1af1cb22a3a
masaIS <fumolife@gmail.com>
2019-06-25 00:56:46 +0900

    commit #1

--- trigger.py
+++ trigger.py
@@ -1,8 +1,9 @@
 #!/user/bin/python3
 
 import os
-import glob
 import ftplib
+import dateutil
+from datetime import datetime, timedelta
 
 def ftp_login(ftp, user, password):
     """ Login FTP server
@@ -45,17 +46,19 @@ def ftp_cwd(ftp, chgdir):
         print('FTP error:', err)
         ftp.close()
 
-def ftp_transfer(ftp):
+def ftp_transfer(ftp, filename):
     """ Transfer file to FTP server
         
         Arguments :
          ftp: FTP class instance
+         filename: temporary filename
         Returns :
          none
 
     """
+    # transfer temp.tmp
     try:
-        tx_file = os.path.join(os.getcwd() + '\\triggers\\temp.tmp')
+        tx_file = os.path.join(os.getcwd() + "\\" + filename)
         #print(tx_file)        
         with open(tx_file, 'rb') as fp:
             res = ftp.storbinary('STOR ' + os.path.basename(tx_file), fp)
@@ -65,16 +68,34 @@ def ftp_transfer(ftp):
         print('FTP error:', err)
         ftp.close()
 
-def ftp_rename(ftp):
-    """ Rename temporary file at FTP server
 
-        Arguments :
-         ftp: FTP class instace
+def ftp_rename(ftp, filename):
+    """ Rename file on FTP server
 
-        Returns :
+        Argument :
+         ftp: FTP class instance
+         filename: temporary filename
+        Return :
          none
-         
+
     """
+    # rename
+    try:
+        # get filelist
+        filelist = ftp.nlst(".")
+        #print(filelist)
+        for file in filelist:
+            if file == filename:
+                res = ftp.rename(filename, 'new.tgr')
+                print('FTP rename:', res)
+            else:
+                continue
+
+    except ftplib.all_errors as err:
+        print('FTP error:', err)
+        ftp.close()
+    
+
 
 
 def ftp_logout(ftp):
@@ -94,11 +115,23 @@ def ftp_logout(ftp):
         print('FTP error:', err)
         ftp.close()
 
-# Create FTP Instance
-ftp_ins = ftplib.FTP('192.168.31.1')
 
-ftp_login(ftp_ins, 'gnss', 'gnss4600')
-ftp_cwd(ftp_ins, 'trig')
-ftp_transfer(ftp_ins)
+if __name__ == "__main__":
+    #pass
+    fmt = '%Y%m%d%H%M%S'
+    stt_dt = datetime.now()
+    
+    print(stt_dt.strftime(fmt), type(stt_dt))
 
-ftp_logout(ftp_ins)
+
+
+
+    # Create FTP Instance
+    ftp_ins = ftplib.FTP('192.168.56.101')
+
+    ftp_login(ftp_ins, 'ftpuser', 'p@ssword123')
+    ftp_cwd(ftp_ins, 'trig')
+    ftp_transfer(ftp_ins, 'tmp_trigger')
+    ftp_rename(ftp_ins, 'tmp_trigger')
+
+    ftp_logout(ftp_ins)
